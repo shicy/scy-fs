03c30a57350c55b489a945595af2b025233f3029
shicy <shicy85@163.com>
2020-10-29 17:23:07 +0800

    测试代码

---
+++ fs_service/src/test/java/org/scy/fs/MainTest.java
@@ -0,0 +1,10 @@
+package org.scy.fs;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        String ids = "0,1,4,6,232,5432,0";
+        System.out.println(ids.substring(0, ids.length() - 1));
+    }
+
+}
