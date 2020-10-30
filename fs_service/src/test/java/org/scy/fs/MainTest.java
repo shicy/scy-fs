21eb2b4b76a5683a7ca7f7966fad81399f4b4017
shicy <shicy85@163.com>
2020-10-30 18:09:47 +0800

    测试

--- fs_service/src/test/java/org/scy/fs/MainTest.java
+++ fs_service/src/test/java/org/scy/fs/MainTest.java
@@ -1,10 +1,14 @@
 package org.scy.fs;
 
+import org.scy.common.utils.StringUtilsEx;
+
 public class MainTest {
 
     public static void main(String[] args) {
         String ids = "0,1,4,6,232,5432,0";
         System.out.println(ids.substring(0, ids.length() - 1));
+
+        System.out.println(StringUtilsEx.getRandomString(32));
     }
 
 }
